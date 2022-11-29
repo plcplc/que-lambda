12888a195bb72177e339b97cdcb05e3e8b1cc55b
Philip Lykke Carlsen <plcplc@gmail.com>
2022-11-29 08:39:24 +0100

    Attempts with nix flake for ghc 9.4.2

--- flake.nix
+++ flake.nix
@@ -3,18 +3,24 @@
   description = "Nix flake for Que-Lambda development";
 
   inputs = {
-    pkgs = {
+    flake-utils = {
+      url = github:numtide/flake-utils;
+    };
+    nixpkgs = {
       url = github:NixOS/nixpkgs;
     };
   };
 
   outputs =
     { self
-    , pkgs
+    , flake-utils
+    , nixpkgs
     }:
+    flake-utils.lib.eachDefaultSystem (system:
     {
       devShells.default =
         let
+          pkgs = import nixpkgs { inherit system; };
           ghcName = "ghc942";
         in
         pkgs.mkShell {
@@ -25,13 +31,13 @@
 
             pkgs.haskell.compiler.${ghcName}
             pkgs.haskell.packages.${ghcName}.cabal-install
-            pkgs.haskell.packages.${ghcName}.ghcid
+            # pkgs.haskell.packages.${ghcName}.ghcid
             pkgs.haskell.packages.${ghcName}.haskell-language-server
-            pkgs.haskell.packages.${ghcName}.hlint
-            pkgs.haskell.packages.${ghcName}.hoogle
-            pkgs.haskell.packages.${ghcName}.hspec-discover
-            pkgs.haskell.packages.${ghcName}.ormolu
+            # pkgs.haskell.packages.${ghcName}.hlint
+            # pkgs.haskell.packages.${ghcName}.hoogle
+            #pkgs.haskell.packages.${ghcName}.hspec-discover
+            # pkgs.haskell.packages.${ghcName}.ormolu
           ];
-        }
-    }
+        };
+    });
 }
